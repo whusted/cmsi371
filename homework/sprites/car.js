eec8619c1702b66fcceb79695a1f910bafd9da8a
ThatHalfKorean <gkim6@lion.lmu.edu>
2014-01-28 14:32:28 -0800

    Made body and steering wheel of car sexier.

--- homework/sprites/car.js
+++ homework/sprites/car.js
@@ -12,8 +12,11 @@ $(function () {
         sprites.renderingContext.fillStyle = "rgb(" + 90 + "," + 90 + "," + 100 + ")";
         sprites.renderingContext.strokeStyle = "black";
         sprites.renderingContext.beginPath();
-        sprites.renderingContext.strokeRect(sprites.car.x, sprites.car.y, 220, 110);
-        sprites.renderingContext.fillRect(sprites.car.x, sprites.car.y, 220, 110);
+        //sprites.renderingContext.strokeRect(sprites.car.x, sprites.car.y, 220, 110);
+		//sprites.renderingContext.strokeRect(sprites.car.x - 50, sprites.car.y + 60, 50, 50);
+		sprites.renderingContext.fillRect(sprites.car.x - 50, sprites.car.y + 60, 50, 50);
+        sprites.renderingContext.fillRect(sprites.car.x, sprites.car.y, 190, 110);
+		sprites.renderingContext.fillRect(sprites.car.x + 180, sprites.car.y - 20, 30, 20);
 
         
 
@@ -28,8 +31,9 @@ $(function () {
         sprites.renderingContext.fillStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
         sprites.renderingContext.strokeStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
         sprites.renderingContext.moveTo(sprites.car.x + 55, sprites.car.y + 50);
-        sprites.renderingContext.bezierCurveTo(sprites.car.x + 75, sprites.car.y + 85, sprites.car.x + 25, sprites.car.y + 60, sprites.car.x + 55, sprites.car.y + 50);
+        sprites.renderingContext.arc(sprites.car.x + 50, sprites.car.y + 50, 5, 0, Math.PI * 2);
         sprites.renderingContext.stroke();
+
 
         //Wheels
         sprites.renderingContext.fillStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
