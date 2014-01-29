dd02a97e5e82e73432cbe460d2fb86c70d4f4879
Willy Husted <willyhusted@comcast.net>
2014-01-29 12:12:01 -0800

    Put separate drawings into own functions

--- homework/sprites/car.js
+++ homework/sprites/car.js
@@ -7,31 +7,47 @@ $(function () {
     
     
     window['sprites'].drawCar = function () {
-        // Body of car
+        sprites.drawBody();
+        sprites.drawWindshield();
+        sprites.drawSteeringWheel();
+        sprites.drawWheels();
+        sprites.drawHubcaps();
+    };
+
+    window['sprites'].drawBody = function () {
         sprites.renderingContext.save();
         sprites.renderingContext.fillStyle = "rgb(" + 90 + "," + 90 + "," + 100 + ")";
         sprites.renderingContext.strokeStyle = "black";
         sprites.renderingContext.beginPath();
-		sprites.renderingContext.fillRect(sprites.car.x - 50, sprites.car.y + 60, 50, 50);
+        sprites.renderingContext.fillRect(sprites.car.x - 50, sprites.car.y + 60, 50, 50);
         sprites.renderingContext.fillRect(sprites.car.x, sprites.car.y, 190, 110);
-		sprites.renderingContext.fillRect(sprites.car.x + 180, sprites.car.y - 20, 30, 20);
+        sprites.renderingContext.fillRect(sprites.car.x + 180, sprites.car.y - 20, 30, 20);
+        sprites.renderingContext.restore();
+    };
 
-        // Front windshield
+    window['sprites'].drawWindshield = function () {
+        sprites.renderingContext.save();
         sprites.renderingContext.fillStyle = "rgb(" + 240 + "," + 240 + "," + 240 + ")";
         sprites.renderingContext.strokeStyle = "white";
         sprites.renderingContext.beginPath();
         sprites.renderingContext.strokeRect(sprites.car.x, sprites.car.y, 70, 60);
         sprites.renderingContext.fillRect(sprites.car.x, sprites.car.y, 70, 60);
+        sprites.renderingContext.restore();
+    };
 
-        //Steering wheel
+    window['sprites'].drawSteeringWheel = function() {
+        sprites.renderingContext.save();
         sprites.renderingContext.fillStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
         sprites.renderingContext.strokeStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
         sprites.renderingContext.moveTo(sprites.car.x + 60, sprites.car.y + 50);
         sprites.renderingContext.arc(sprites.car.x + 50, sprites.car.y + 50, 10, 0, Math.PI * 2);
         sprites.renderingContext.arc(sprites.car.x + 50, sprites.car.y + 50, 12, 0, Math.PI * 2);
         sprites.renderingContext.stroke();
+        sprites.renderingContext.restore();
+    };
 
-        //Wheels
+    window['sprites'].drawWheels = function() {
+        sprites.renderingContext.save();
         sprites.renderingContext.fillStyle = "rgb(" + 10 + "," + 10 + "," + 10 + ")";
         sprites.renderingContext.beginPath();
         sprites.renderingContext.arc(sprites.car.x + 50, sprites.car.y + 110, 30, 0, Math.PI * 2);
@@ -40,8 +56,11 @@ $(function () {
         sprites.renderingContext.fill();
         sprites.renderingContext.stroke();
         sprites.renderingContext.restore();
-        
-        //Hubcaps
+
+    };
+
+    window['sprites'].drawHubcaps = function() {
+        sprites.renderingContext.save();
         sprites.renderingContext.fillStyle = "rgb(" + 210 + "," + 210 + "," + 210 + ")";
         sprites.renderingContext.beginPath();
         sprites.renderingContext.arc(sprites.car.x + 50, sprites.car.y + 110, 15, 0, Math.PI * 2);
@@ -50,7 +69,6 @@ $(function () {
         sprites.renderingContext.fill();
         sprites.renderingContext.stroke();
         sprites.renderingContext.restore();
-
     };
 
     
