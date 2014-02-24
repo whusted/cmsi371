a27be622d1c30b1f8b1e28a1e3cceba4c8f18519
Willy Husted <willyhusted@comcast.net>
2014-02-23 20:53:44 -0800

    Bazaar code copied and lighten filter added to neighborhood

---
+++ homework/nanoshop-filters/nanoshop-neighborhood/nanoshop-neighborhood-demo.js
@@ -0,0 +1,98 @@
+/*
+ * This demo script uses the NanoshopNeighborhood module to apply a
+ * "pixel neighborhood" filter on a canvas drawing.
+ */
+(function () {
+    var canvas = $("#picture")[0],
+        renderingContext = canvas.getContext("2d"),
+        gradient;
+
+    // Some edge lines to test for wraparound bleeding.
+    renderingContext.strokeStyle = "yellow";
+    renderingContext.beginPath();
+    renderingContext.moveTo(0, 0);
+    renderingContext.lineTo(canvas.width - 1, 0);
+    renderingContext.stroke();
+
+    renderingContext.strokeStyle = "cyan";
+    renderingContext.beginPath();
+    renderingContext.moveTo(0, canvas.height - 1);
+    renderingContext.lineTo(canvas.width - 1, canvas.height - 1);
+    renderingContext.stroke();
+
+    renderingContext.strokeStyle = "green";
+    renderingContext.beginPath();
+    renderingContext.moveTo(0, 0);
+    renderingContext.lineTo(0, canvas.height - 1);
+    renderingContext.stroke();
+
+    renderingContext.strokeStyle = "red";
+    renderingContext.beginPath();
+    renderingContext.moveTo(canvas.width - 1, 0);
+    renderingContext.lineTo(canvas.width - 1, canvas.height / 2);
+    renderingContext.stroke();
+
+    renderingContext.strokeStyle = "blue";
+    renderingContext.beginPath();
+    renderingContext.moveTo(canvas.width - 1, canvas.height / 2);
+    renderingContext.lineTo(canvas.width - 1, canvas.height - 1);
+    renderingContext.stroke();
+
+    // Adapted from original code by Tyler Nichols.
+    gradient = renderingContext.createRadialGradient(120, 120, 15, 120, 120, 75);
+    gradient.addColorStop(0, "rgb(255, 102, 102)");
+    gradient.addColorStop(1, "red");
+
+    // Draw the sphere with a radial gradient.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = gradient;
+    renderingContext.arc(150, 150, 75, 0, 2 * Math.PI, true);
+    renderingContext.shadowColor = "gray";
+    renderingContext.shadowBlur = 20;
+    renderingContext.shadowOffsetX = 10;
+    renderingContext.shadowOffsetY = 15;
+    renderingContext.fill();
+    renderingContext.closePath();
+
+    // Draw the top of the cube.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = "rgb(140, 140, 140)";
+    renderingContext.moveTo(300, 300);
+    renderingContext.lineTo(335, 265);
+    renderingContext.lineTo(435, 265);
+    renderingContext.lineTo(400, 300);
+    renderingContext.lineTo(300, 300);
+    renderingContext.fill();
+    renderingContext.closePath();
+
+    // Draw the face of the cube.
+    renderingContext.fillStyle = "rgb(110, 110, 110)";
+    renderingContext.fillRect(300, 300, 100, 100);
+
+    // Draw the right side of the cube.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = "rgb(79, 79, 79)";
+    renderingContext.moveTo(435, 265);
+    renderingContext.lineTo(435, 355);
+    renderingContext.lineTo(400, 400);
+    renderingContext.lineTo(400, 300);
+    renderingContext.lineTo(435, 265);
+    renderingContext.fill();
+    renderingContext.closePath();
+    // (end of adapted code by Tyler Nichols)
+
+    // Set a little event handler to apply the filter.
+    $("#apply-filter-button").click(function () {
+        // Filter time.
+        renderingContext.putImageData(
+            NanoshopNeighborhood.applyFilter(
+                renderingContext,
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                NanoshopNeighborhood.lighten
+                //NanoshopNeighborhood.darken
+                //NanoshopNeighborhood.averager // Convenience comment for easy switching.
+            ),
+            0, 0
+        );
+    });
+}());
---
+++ homework/nanoshop-filters/nanoshop-neighborhood/nanoshop-neighborhood.js
@@ -0,0 +1,134 @@
+/*
+ * This is a very simple module that demonstrates rudimentary,
+ * pixel-level image processing using a pixel's "neighborhood."
+ */
+var NanoshopNeighborhood = {
+    /*
+     * A basic "darkener"---this one does not even use the entire pixel neighborhood;
+     * just the exact current pixel like the original Nanoshop.
+     */
+    darkener: function (rgbaNeighborhood) {
+        return [
+            rgbaNeighborhood[4].r / 2,
+            rgbaNeighborhood[4].g / 2,
+            rgbaNeighborhood[4].b / 2,
+            rgbaNeighborhood[4].a
+        ];
+    },
+
+    lighten: function (rgbaNeighborhood) {
+        return [
+            (rgbaNeighborhood[4].r + 5) * 1.1,
+            (rgbaNeighborhood[4].g + 5) * 1.1,
+            (rgbaNeighborhood[4].b + 5) * 1.1,
+            rgbaNeighborhood[4].a
+        ];
+    },
+
+    /*
+     * A basic "averager"---this one returns the average of all the pixels in the
+     * given neighborhood.
+     */
+    averager: function (rgbaNeighborhood) {
+        var rTotal = 0,
+            gTotal = 0,
+            bTotal = 0,
+            aTotal = 0,
+            i;
+
+        for (i = 0; i < 9; i += 1) {
+            rTotal += rgbaNeighborhood[i].r;
+            gTotal += rgbaNeighborhood[i].g;
+            bTotal += rgbaNeighborhood[i].b;
+            aTotal += rgbaNeighborhood[i].a;
+        }
+
+        return [ rTotal / 9, gTotal / 9, bTotal / 9, aTotal / 9 ];
+    },
+
+    /*
+     * Applies the given filter to the given ImageData object,
+     * then modifies its pixels according to the given filter.
+     *
+     * A filter is a function ({r, g, b, a}[9]) that returns another
+     * color as a 4-element array representing the new RGBA value
+     * that should go in the center pixel.
+     */
+    applyFilter: function (renderingContext, imageData, filter) {
+        // For every pixel, replace with something determined by the filter.
+        var result = renderingContext.createImageData(imageData.width, imageData.height),
+            i,
+            j,
+            max,
+            iAbove,
+            iBelow,
+            pixel,
+            pixelColumn,
+            firstRow,
+            lastRow,
+            rowWidth = imageData.width * 4,
+            sourceArray = imageData.data,
+            destinationArray = result.data,
+
+            // A convenience function for creating an rgba object.
+            rgba = function (startIndex) {
+                return {
+                    r: sourceArray[startIndex],
+                    g: sourceArray[startIndex + 1],
+                    b: sourceArray[startIndex + 2],
+                    a: sourceArray[startIndex + 3]
+                };
+            };
+
+        for (i = 0, max = imageData.width * imageData.height * 4; i < max; i += 4) {
+            // The 9-color array that we build must factor in image boundaries.
+            // If a particular location is out of range, the color supplied is that
+            // of the extant pixel that is adjacent to it.
+            iAbove = i - rowWidth;
+            iBelow = i + rowWidth;
+            pixelColumn = i % rowWidth;
+            firstRow = sourceArray[iAbove] === undefined;
+            lastRow = sourceArray[iBelow] === undefined;
+
+            pixel = filter([
+                // The row of pixels above the current one.
+                firstRow ?
+                    (pixelColumn ? rgba(i - 4) : rgba(i)) :
+                    (pixelColumn ? rgba(iAbove - 4) : rgba(iAbove)),
+
+                firstRow ? rgba(i) : rgba(iAbove),
+
+                firstRow ?
+                    ((pixelColumn < rowWidth - 4) ? rgba(i + 4) : rgba(i)) :
+                    ((pixelColumn < rowWidth - 4) ? rgba(iAbove + 4) : rgba(iAbove)),
+
+                // The current row of pixels.
+                pixelColumn ? rgba(i - 4) : rgba(i),
+
+                // The center pixel: the filter's returned color goes here
+                // (based on the loop, we are at least sure to have this).
+                rgba(i),
+
+                (pixelColumn < rowWidth - 4) ? rgba(i + 4) : rgba(i),
+
+                // The row of pixels below the current one.
+                lastRow ?
+                    (pixelColumn ? rgba(i - 4) : rgba(i)) :
+                    (pixelColumn ? rgba(iBelow - 4) : rgba(iBelow)),
+
+                lastRow ? rgba(i) : rgba(iBelow),
+
+                lastRow ?
+                    ((pixelColumn < rowWidth - 4) ? rgba(i + 4) : rgba(i)) :
+                    ((pixelColumn < rowWidth - 4) ? rgba(iBelow + 4) : rgba(iBelow))
+            ]);
+
+            // Apply the color that is returned by the filter.
+            for (j = 0; j < 4; j += 1) {
+                destinationArray[i + j] = pixel[j];
+            }
+        }
+
+        return result;
+    }
+};
