9fe221bd265c16d14adcfc99af848c8fe1d3e6c6
Willy Husted <willyhusted@comcast.net>
2014-03-31 21:51:02 -0700

    Multiplication implemented but untested

--- homework/pipeline/matrix4x4/matrix4x4.js
+++ homework/pipeline/matrix4x4/matrix4x4.js
@@ -17,6 +17,27 @@ var Matrix4x4 = (function() {
     matrix4x4.prototype.dimensions = function () {
         return this.elements.length;
     };
+    
+
+    matrix4x4.prototype.multiply = function (m) {
+        var result = new Matrix4x4,
+            row,
+            col,
+            sum;
+
+        for (row = 0; row < 4; row++) {
+            for (col = 0; col < 4; col++) {
+            	// reset the indivdual sum each iteration
+            	sum = 0;
+            	for (i = 0; i < 4; i++) {
+		            sum += this.elements[row * 4 + i] * m.elements[i * 4 + col];
+		        }
+		        result.elements[row * 4 + col] = sum;
+            }
+        }
+
+        return result;
+    };
 
 
 
