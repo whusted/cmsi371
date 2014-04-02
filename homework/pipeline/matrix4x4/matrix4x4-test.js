f7e59611cbb3485c71290c0e7036597285346643
Willy Husted <willyhusted@comcast.net>
2014-04-02 13:00:55 -0700

    error checked multiplication and tested scale, both successfully

--- homework/pipeline/matrix4x4/matrix4x4-test.js
+++ homework/pipeline/matrix4x4/matrix4x4-test.js
@@ -71,6 +71,16 @@ $(function () {
         equal(m3.elements[14], 9, "Fifteenth element by index");
         equal(m3.elements[15], 3, "Sixteenth element by index");
 
+        m2 = new Matrix4x4(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 0, 0, 0, 1);
+
+        throws(
+            function () {
+                return m1.multiply(m2);
+            },
+            "Check for non-square matrices"
+        );
+
+
     });
 
     test("Transformations", function() {
@@ -95,6 +105,24 @@ $(function () {
         equal(m.elements[15], 1, "Sixteenth element by index");
 
 
+        m = Matrix4x4.getScaleMatrix(3, 1, 4);
+        equal(m.dimensions(), 16, "Matrix size");
+        equal(m.elements[0], 3, "First element by index");
+        equal(m.elements[1], 0, "Second element by index");
+        equal(m.elements[2], 0, "Third element by index");
+        equal(m.elements[3], 0, "Fourth element by index");
+        equal(m.elements[4], 0, "Fifth element by index");
+        equal(m.elements[5], 1, "Sixth element by index");
+        equal(m.elements[6], 0, "Seventh element by index");
+        equal(m.elements[7], 0, "Eighth element by index");
+        equal(m.elements[8], 0, "Ninth element by index");
+        equal(m.elements[9], 0, "Tenth element by index");
+        equal(m.elements[10], 4, "Eleventh element by index");
+        equal(m.elements[11], 0, "Twelfth element by index");
+        equal(m.elements[12], 0, "Thirteenth element by index");
+        equal(m.elements[13], 0, "Fourteenth element by index");
+        equal(m.elements[14], 0, "Fifteenth element by index");
+        equal(m.elements[15], 1, "Sixteenth element by index");
 
     });
    
