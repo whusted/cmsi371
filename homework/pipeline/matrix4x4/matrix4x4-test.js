7c90207610521808809ff9d832284fea175d9c66
Willy Husted <willyhusted@comcast.net>
2014-04-02 20:54:24 -0700

    Frustum test added

--- homework/pipeline/matrix4x4/matrix4x4-test.js
+++ homework/pipeline/matrix4x4/matrix4x4-test.js
@@ -192,6 +192,31 @@ $(function () {
 
 
     });
-   
+
+    test("Projections", function() {
+        var m = Matrix4x4.getFrustumMatrix(2, -2, 2, -2, -2, 2);
+        console.log(m.elements);
+
+        equal(m.dimensions(), 16, "Matrix size");
+        equal(m.elements[0], -1, "First element by index");
+        equal(m.elements[1], 0, "Second element by index");
+        equal(m.elements[2], 0, "Third element by index");
+        equal(m.elements[3], 0, "Fourth element by index");
+        equal(m.elements[4], 0, "Fifth element by index");
+        equal(m.elements[5], -1, "Sixth element by index");
+        equal(m.elements[6], 0, "Seventh element by index");
+        equal(m.elements[7], 0, "Eighth element by index");
+        equal(m.elements[8], 0, "Ninth element by index");
+        equal(m.elements[9], 0, "Tenth element by index");
+        equal(m.elements[10], 0, "Eleventh element by index");
+        equal(m.elements[11], 2, "Twelfth element by index");
+        equal(m.elements[12], 0, "Thirteenth element by index");
+        equal(m.elements[13], 0, "Fourteenth element by index");
+        equal(m.elements[14], -1, "Fifteenth element by index");
+        equal(m.elements[15], 0, "Sixteenth element by index");
+
+
+    });
+    
 
 });
