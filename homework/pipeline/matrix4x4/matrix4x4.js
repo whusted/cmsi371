678018d3750a170fc130629c851a1a8a7c7f2f77
whusted <whusted@lion.lmu.edu>
2014-05-10 09:15:36 -0700

    Fixed tab issue in matrix lib

--- homework/pipeline/matrix4x4/matrix4x4.js
+++ homework/pipeline/matrix4x4/matrix4x4.js
@@ -8,12 +8,12 @@ var Matrix4x4 = (function() {
               0, 0, 0, 1 ];
     },
 
-    // JD: You know what I'm going to say here right?
-	    checkDimensions = function (m1, m2) {
-	            if (m1.dimensions() !== m2.dimensions()) {
-	                throw "Matrices have different dimensions";
-	            }
-	    };
+    // (fixed) JD: You know what I'm going to say here right?
+    checkDimensions = function (m1, m2) {
+            if (m1.dimensions() !== m2.dimensions()) {
+                throw "Matrices have different dimensions";
+            }
+    };
 
     matrix4x4.prototype.dimensions = function () {
         return this.elements.length;
